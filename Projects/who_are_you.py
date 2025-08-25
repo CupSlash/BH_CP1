4b4f04ed157962c05139cb51a317782092ba7520
CupSlash <briggs.hardy@ucas-edu.net>
2025-08-25 10:04:11 -0600

    who yo

---
+++ Projects/who_are_you.py
@@ -0,0 +1,5 @@
+# BH 2nd who_are_you
+name =input("what's your name: ")
+age =input("How old are you: ")
+color =input("What's you favorite color: ")
+print("Wow", name, "San,", "you're only", age, "? I love", color, "too!")
