d6e725b954f19ae2e0a2cb49997c1219117a757b
CupSlash <briggs.hardy@ucas-edu.net>
2025-09-03 18:01:27 -0600

    removed adjective and noun 2

--- Projects/madlib.py
+++ Projects/madlib.py
@@ -3,15 +3,13 @@
 adjective1 = input("Give me an adjective: ")
 plural_noun1 = input("What's an example of a plural noun: ")
 noun1 = input("I need a noun: ")
-adjective2 = input("Wait a minute... I think I need a new adjective: ")
 plural_noun2 = input("Sorry, but I also need a new plural noun: ")
 number = input("Give me a number, 1-100: ")
-noun2 = input("I need a second noun: ")
 
 sentence1 = "\nThis morning, my " + adjective1 + " alarm clock didn't go off, so I rushed out the door wearing " + plural_noun1 + " on my feet."
-sentence2 = " At school my " + noun1 + " exploded during chemistry class, which caused a " + adjective2 + " mess."
+sentence2 = " At school my " + noun1 + " exploded during chemistry class, which caused a " + adjective1 + " mess."
 sentence3 = " During lunch, someone traded me their " + plural_noun2 + " for my mystery meat."
-sentence4 = " In gym, we had to do a " + number + " laps around the track, and I almost passed out."
-sentence5 = " By the end of the day, I felt like a " + noun2 + " that had been run over by a bus."
+sentence4 = " In gym, we had to do " + number + " laps around the track, and I almost passed out."
+sentence5 = " By the end of the day, I felt like a " + noun1 + " that had been run over by a bus."
 story = sentence1 + sentence2 + sentence3 + sentence4 + sentence5
 print(story)
