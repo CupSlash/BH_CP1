b67b3e8a39568d38c86053829dc1a5e7bebf7856
CupSlash <briggs.hardy@ucas-edu.net>
2025-09-30 10:49:03 -0600

    finished list manager

--- Projects/list_manager.py
+++ Projects/list_manager.py
@@ -3,17 +3,19 @@
 list = []
 
 while True:
-    action = input("What do you want to do? Your options are, 'exit', 'add item' 'remove item'")
+    action = input("What do you want to do? Your options are, 'print list', 'exit', 'add item' 'remove item'")
     if action == "add item":
         new_item = input("What item would you like to add?").strip()
         list.append(new_item)
         print(f"Here is your new Shopping list:{list}")
+    elif action == "print list":
+        print(f"Here is your Shopping list {list}")
     elif action == "remove item":
         print(list)
         removed_item = input("What item would you like to remove?").strip()
         if removed_item not in list:
             print("This item is not in the shopping list, maybe you spelled something wrong?")
-        elif:
+        elif removed_item in list:
             list.remove(removed_item)
             print(f"Here is your new Shopping list: {list}")
         else:
@@ -21,4 +23,4 @@ while True:
     elif action == "exit":
         break
     else:
-        print("That item is not a possible choice, please try again.")
+        print("That is not a given action. Please enter one of the possible choices below.")
