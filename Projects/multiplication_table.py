4785ad051baa0545efe28144a465ddfb4038d71f
CupSlash <briggs.hardy@ucas-edu.net>
2025-10-01 10:51:00 -0600

    Unfinished Multiplication Table

---
+++ Projects/multiplication_table.py
@@ -0,0 +1,12 @@
+#BH 2nd Multiplication Table
+
+
+table = [x,1,2,3,4,5,6,7,8,9,10,11,12
+         1,1,2,3,4,5,6,7,8,9,10,11,12
+         2,2,4,6,8,10,12,14,16,18,20,22,24
+         3,3,6,9]
+
+for This_technically_counts_mslarose in table:
+    pass
+
+print(table)
