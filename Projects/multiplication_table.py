51a2a3b88a2d832aa6631550e944071c0d3458a2
CupSlash <briggs.hardy@ucas-edu.net>
2025-10-02 10:42:44 -0600

    Multiplication Table

--- Projects/multiplication_table.py
+++ Projects/multiplication_table.py
@@ -1,12 +1,8 @@
 #BH 2nd Multiplication Table
 
+table = ()
 
-table = [x,1,2,3,4,5,6,7,8,9,10,11,12
-         1,1,2,3,4,5,6,7,8,9,10,11,12
-         2,2,4,6,8,10,12,14,16,18,20,22,24
-         3,3,6,9]
-
-for This_technically_counts_mslarose in table:
-    pass
-
-print(table)
+for table in range (1,13):
+    for x in range (1,13):
+        print(f"{table * x:4}", end="")
+    print()
