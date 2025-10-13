b618e0de51db7ea3f268200227e41c665356fc45
CupSlash <briggs.hardy@ucas-edu.net>
2025-10-13 10:50:27 -0600

    Potentially finished.

--- Projects/password_strength_checker.py
+++ Projects/password_strength_checker.py
@@ -1,28 +1,41 @@
 #BH 2nd Password Strength Checker
 
 # total = 0
-total = 0
+total = 1
 # password = user's input after asking password
 password = input("What is your password? :")
 # if len(password) == 8:
 if len(password) >= 8:
     #total = total + 1
     total = total + 1
+else:
+    pass
 #check to see if the user has one or more uppercase and lowercase letter with .upper and .lower [add point to total]
-elif password.isupper() == True:
+if password.isupper() == True:
     total = total + 1
-elif password.islower() == True:
+else:
+    pass
+if password.islower() == True:
     total = total + 1
+else:
+    pass
 #check for special character with .alnum[add point to total]
-elif password.isalnum() == True:
-    total = total + 1
+if password.isalnum() == True:
+    total = total - 1
+else:
+    pass
 #check for any number with .isdigit[add point to total]
-elif password.isdigit() == True:
+if password.isdigit() == True:
     total = total + 1
+else:
+    pass
 #print the total out of 5 for the user and how strong it is
-print ("Your password strength is a", total, "out of 5")
+if password == "password":
+    print("Ain't no way your password is password.")
+else:
+    print ("Your password strength is a", total, "out of 5")
 #find out how good the total is with an if statement
-if total == 0 or 1 or 2:
+if total <= 2:
     print("Your password is pretty weak.")
 elif total == 3:
     print("I'd say your password is average.")
