b6ff2c7ddd0fc85cb6855c0cd971a4b3665a7bef
CupSlash <briggs.hardy@ucas-edu.net>
2025-10-14 10:42:10 -0600

    PasswordStrengthChecker

--- Projects/password_strength_checker.py
+++ Projects/password_strength_checker.py
@@ -1,7 +1,10 @@
 #BH 2nd Password Strength Checker
-
+upper = False
+lower = False
+number = False
+spe_char = False
 # total = 0
-total = 1
+total = 0
 # password = user's input after asking password
 password = input("What is your password? :")
 # if len(password) == 8:
@@ -11,24 +14,40 @@ if len(password) >= 8:
 else:
     pass
 #check to see if the user has one or more uppercase and lowercase letter with .upper and .lower [add point to total]
-if password.isupper() == True:
-    total = total + 1
-else:
-    pass
-if password.islower() == True:
-    total = total + 1
-else:
-    pass
+for char in password:
+    if char.isupper():
+        upper = True
+    elif char.islower():
+        lower = True
+    elif char.isdigit():
+        number = True
+    elif not char.isalnum():
+        spe_char = True
+    else:
+        pass
 #check for special character with .alnum[add point to total]
-if password.isalnum() == True:
-    total = total - 1
-else:
-    pass
+#if password.isalnum() == True:
+   # total = total - 1
+#else:
+    #pass
 #check for any number with .isdigit[add point to total]
-if password.isdigit() == True:
-    total = total + 1
-else:
-    pass
+#if password.isdigit() == True:
+    #total = total + 1
+#else:
+    #pass
+
+if upper == True:
+    total += 1
+
+if lower == True:
+    total += 1
+
+if number == True:
+    total += 1
+
+if spe_char == True:
+    total += 1
+
 #print the total out of 5 for the user and how strong it is
 if password == "password":
     print("Ain't no way your password is password.")
