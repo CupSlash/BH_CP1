073ce156c609a6066f34cacf5b24213c79f2982c
CupSlash <briggs.hardy@ucas-edu.net>
2025-10-22 17:31:51 -0600

    caesar_cypher completed 1

--- Projects/caesar_cypher.py
+++ Projects/caesar_cypher.py
@@ -4,40 +4,53 @@
 #The output should change based on user input and chosen operation
 #Outputs need to be easy to read and understand
 code = []
+game = False
 encode = False
 decode = False
 
 
-choice = input("Hello, would you like to encode a message or decode a message?")
-if choice == "encode":
-    encode = True
-elif choice == "decode":
-    decode = True
-else:
-    print("That isn't an option. Please type either 'encode' or 'decode'")
-#ENCODING
-if encode == True:
-    new_item = input("What item would would you like to encode?")
-    shift_number = int(input("How much would you like to shift your input?"))
-    for char in new_item:
-        if char.isalpha:
-            new_item = ord(f"{char}") + shift_number
-            code_item = chr(new_item)
-            code.append(code_item)
+start_game = input("Would you like to begin? y/n  ")
+if start_game == "y":
+    game = True
+else: 
+    pass
 
-join = ''.join(code)
-
-
-#if input is John it would be Kpio with a shift of 1
-
-
-#DECODING
-if decode == True:
-    print(join)
-    decode_item = input("Which item would you like to decode?")
-    if decode_item not in join:
-        print("That item is not in the list, please choose one of the following items.", join)
-    elif decode_item in join:
-        print("Here is the decoded word.")
-   
-print(join)
+while game == True:
+    choice = input("Hello, would you like to encode a message or decode a message?")
+    if choice == "encode":
+        encode = True
+    elif choice == "decode":
+        decode = True
+    else:
+        print("That isn't an option. Please type either 'encode' or 'decode'")
+    #ENCODING
+    while encode == True:
+        new_item = input("What item would would you like to encode?")
+        shift_number = int(input("How much would you like to shift your input?"))
+        for char in new_item:
+            if char.isalpha:
+                new_item = ord(f"{char}") + shift_number
+                code_item = chr(new_item)
+                code.append(code_item)
+                join = ''.join(code)
+                list = [join]
+        encode = False
+    else:
+        pass
+    #if input is John it would be Kpio with a shift of 1
+    #DECODING
+    while decode == True:
+        print(list)
+        decode_item = input("Which item would you like to decode?")
+        if decode_item not in join:
+            print("That item is not in the list, please choose one of the following items.", list)
+        elif decode_item in join:
+            for char in decode_item:
+                if char.isalpha:
+                    item_2decode = ord(f"{char}") - shift_number
+            print("Here is the decoded word.", item_2decode)
+            list = list.remove(decode_item)
+        print(list)
+        decode = False
+    else:
+        pass
