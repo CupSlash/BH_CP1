34799d4732a8b7427d1b398352ca18b41d251ce2
UCAS-edu.net CupSlash <UCAS-edu.net briggs.hardy@ucas-edu.net>
2025-10-28 10:51:07 -0600

    commit and push

---
+++ Notes/libraries_builtin_functions.py
@@ -0,0 +1,22 @@
+#BH 2nd Libraries and Built-in Functions
+import random
+import turtle
+number = random.randint(100, 500)
+
+turtle.shape("turtle")
+turtle.color("#F417CF")
+turtle.pensize(20)
+turtle.fillcolor("blue")
+turtle.begin_fill()
+for x in range(4):
+    turtle.forward(250)
+    turtle.right(90)
+
+turtle.end_fill()
+
+turtle.penup()
+turtle.goto(-50, 100)
+turtle.pendown()
+
+turtle.done()
+
---
+++ Projects/race_turtles.py
@@ -0,0 +1,23 @@
+#BH 2nd Turtle Racing
+
+#Import the turtle and random libraries at the beginning
+
+import turtle
+import random
+
+#Create a game with 5 turtles, each a different color
+
+
+Red_turtle = turtle.shape("turtle")
+Orange_turtle = turtle.shape("turtle")
+Yellow_turtle = turtle.shape("turtle")
+Green_turtle = turtle.shape("turtle")
+Indigo_turtle = turtle.shape("turtle")
+
+#Use a loop to move turtles forward a random number of steps each round
+#Clearly mark the finish line on the screen
+#Announce the winner either on the turtle screen or in the terminal
+#Make sure the turtles' movement and race logic are correctly implemented
+#Use functions to organize code, e.g., one for setting up the race, one for moving turtles, and one for determining the winner
+#Test the game to ensure it runs smoothly and correctly identifies the winner (minimum of 5 times)
+#Commit and push your code to Github
