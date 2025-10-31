23a8dd8923fa7ca2ff3e0057047bec1a8ad5f883
UCAS-edu.net CupSlash <UCAS-edu.net briggs.hardy@ucas-edu.net>
2025-10-31 10:50:50 -0600

    HOW DO I DO THIS

---
+++ Projects/maze_generator.py
@@ -0,0 +1,45 @@
+#BH 2nd maze_generator.py
+
+import random
+import turtle
+screen = turtle.Screen()
+screen.setup(1000,1000)
+
+#Maze needs to be at least 6x6 
+
+def maze_edge():
+    maze_artist = turtle.Turtle()
+    maze_artist.penup()
+    maze_artist.setpos(100,100)
+    maze_artist.pendown()
+    maze_artist.forward(600)
+    maze_artist.right(90)
+    maze_artist.forward(600)
+    maze_artist.right(90)
+    maze_artist.forward(600)
+    maze_artist.right(90)
+    maze_artist.forward(600)
+    maze_artist.right(90)
+maze_edge()
+
+#se nested loops to iterate through a grid system for maze creation
+#Implement conditionals to determine wall placement
+#Ensure that the maze has at least one valid path from start to finish
+#Use functions to organize code, e.g., one for setup, one for drawing walls, and one for generating the maze layout
+#Make sure the maze generation logic is correctly implemented
+#Test the program to ensure it consistently generates valid, solvable mazes
+#Clearly mark the start and end points of the maze
+
+
+
+
+let_1 = 
+
+line1 = (let_1, let_2, let_3, let_4, let_5, let_6)
+line2 = (let_7, let_8, let_9, let_10, let_11, let_12)
+line3 = (let_13, let_14, let_15, let_16, let_17, let_18)
+line4 = (let_19, let_20, let_21, let_22, let_23, let_24)
+line5 = (let_25, let_26, let_27, let_28, let_29, let_30)
+line6 = (let_31, let_32, let_33, let_34, let_35, let_36)
+
+turtle.done()
