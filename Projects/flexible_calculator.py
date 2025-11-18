5888831ff03e7ac4d1b84cb12519771393744ec2
CupSlash <briggs.hardy@ucas-edu.net>
2025-11-18 10:17:57 -0700

    testing commit and push on new computer

--- Projects/flexible_calculator.py
+++ Projects/flexible_calculator.py
@@ -1,9 +1,9 @@
-#BH 2nd Flexible Calc.
+#BH 2nd Flexible Calc. 
 #introduce user
 print("Welcome to the flexible calculator!")
-#get their numbers and operatio
+#get their numbers and operation
 operation = input("Which operation would you like to perform? sum, average, max, min, product: ")
 numbers = input("What numbers would you like to use? (type done when done): ")
 if numbers == "done":
     pass
-#If statements? For each operation
+#If statements? For each operation 
